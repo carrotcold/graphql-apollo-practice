f57c51de6c5717051d392885482978ce35fe4269
Soin Na <carrotcold@gmail.com>
2020-12-30 05:19:19 +0900

    create apollo client and use GET_MOVIES query

--- packages/client/src/components/App.js
+++ packages/client/src/components/App.js
@@ -1,13 +1,17 @@
 import { HashRouter as Router, Route } from 'react-router-dom';
+import { ApolloProvider } from '@apollo/client';
+import client from '../apollo';
 import Home from '../routes/Home';
 import Detail from '../routes/Detail';
 
 function App() {
   return (
-    <Router>
-      <Route exact path='/' component={Home} />
-      <Route exact path='/:id' component={Detail} />
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/:id' component={Detail} />
+      </Router>
+    </ApolloProvider>
   );
 }
 
--- packages/client/src/routes/Home.js
+++ packages/client/src/routes/Home.js
@@ -1,1 +1,25 @@
-export default () => 'Home';
+import { gql, useQuery } from '@apollo/client';
+
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
+
+function Home() {
+  const { loading, error, data } = useQuery(GET_MOVIES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return data.movies.map(({ id, medium_cover_image }) => (
+    <div key={id}>
+      <p>{medium_cover_image}</p>
+    </div>
+  ));
+}
+
+export default Home;
